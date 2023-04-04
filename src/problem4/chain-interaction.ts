8353600ff574863b7a40e735d8665b9ee755da94
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2023-04-04 22:35:16 +0800

    Update chain-interaction.ts
    Adjust indentations.

--- src/problem4/chain-interaction.ts
+++ src/problem4/chain-interaction.ts
@@ -17,19 +17,19 @@ const swthTokenContract = new ethers.Contract(swthTokenContractAddress, swthToke
 
 // Stores the addresses to look up.
 const addresses: string[] = [
-  '0xb5d4f343412dc8efb6ff599d790074d0f1e8d430',
-  '0x0020c5222a24e4a96b720c06b803fb8d34adc0af',
-  '0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392',
+    '0xb5d4f343412dc8efb6ff599d790074d0f1e8d430',
+    '0x0020c5222a24e4a96b720c06b803fb8d34adc0af',
+    '0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392',
 ];
 
 // Retrieve the balances and outputs the results.
 async function retrieveBalances() {
-  for (const address of addresses) {
-    // Gets the balance of the address.
-    const balance = await swthTokenContract.balanceOf(address);
-    // Formats the amount for displaying to the user with 8 decimal places as stated on https://bscscan.com/token/0x250b211ee44459dad5cd3bca803dd6a7ecb5d46c
-    console.log(`${address} ${ethers.formatUnits(balance, 8)}`);
-  }
+    for (const address of addresses) {
+        // Gets the balance of the address.
+        const balance = await swthTokenContract.balanceOf(address);
+        // Formats the amount for displaying to the user with 8 decimal places as stated on https://bscscan.com/token/0x250b211ee44459dad5cd3bca803dd6a7ecb5d46c
+        console.log(`${address} ${ethers.formatUnits(balance, 8)}`);
+    }
 }
 
 retrieveBalances();
