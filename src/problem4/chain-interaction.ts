25e9d4688a130d02c3f812de813b87b28920eb71
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2023-04-04 22:17:51 +0800

    Update problem 4 to TypeScript
    Change problem 4 implementation from JavaScript to TypeScript.

--- src/problem4/chain-interaction.ts
+++ src/problem4/chain-interaction.ts
@@ -8,13 +8,13 @@ const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.bina
 const swthTokenContractAddress = '0xc0ecb8499d8da2771abcbf4091db7f65158f1468';
 // Gets the account balance
 const swthTokenContractAbi = [
-    "function balanceOf(address) view returns (uint)"
+    "function balanceOf(address: string) view returns (uint)"
 ];
 // Creates the Contract object.
 const swthTokenContract = new ethers.Contract(swthTokenContractAddress, swthTokenContractAbi, provider);
 
 // Stores the addresses to look up.
-const addresses = [
+const addresses: string[] = [
   '0xb5d4f343412dc8efb6ff599d790074d0f1e8d430',
   '0x0020c5222a24e4a96b720c06b803fb8d34adc0af',
   '0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392',
